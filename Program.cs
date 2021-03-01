65a4eafee8e1f7f6bea9acd30eaebc8847d10fa4
Andrea <chela355@hotmail.com>
2021-03-01 16:12:30 -0500

    se hizo la tarea

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Indice_de_masa_corporal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Ingrese su estatura: ");
+         double estatura=Convert.ToInt32(Console.ReadLine());
+         
+         Console.WriteLine("Cual es su Peso: ");
+         double peso=Convert.ToInt32(Console.ReadLine());
+         
+         Console.WriteLine("Cual es su edad: ");
+         double edad=Convert.ToInt32(Console.ReadLine());
+
+         double IMC= peso / System.Math.Pow(estatura/100,2);
+
+            if (edad < 45 && IMC < 22.0)
+            {
+                Console.WriteLine("Su riesgo de sufrir enfermedades coronarias es Bajo");
+            }
+            else if (edad < 45 && IMC >= 22.0)
+            {
+                Console.WriteLine("Su riesgo de sufrir enfermedades coronarias es Medio");
+            }
+            else if (edad >= 45 && IMC < 22.0)
+            {
+                Console.WriteLine("Su riesgo de sufrir enfermedades coronarias es Medio");
+            }
+            else if (edad >= 45 && IMC >= 22.0)
+            {
+                Console.WriteLine("Su riesgo de sufrir enfermedades coronarias es Alto");
+            }
+
+
+
+
+
+        }
+    }
+}
